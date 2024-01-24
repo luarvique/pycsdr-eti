7961ca49f3bd67a3e1384b62d6990abce3e7e10c
Jakob Ketterl <jakob.ketterl@gmx.de>
2024-01-24 22:35:26 +0100

    send programme info as dictionary

--- src/pickleserializer.cpp
+++ src/pickleserializer.cpp
@@ -66,3 +66,90 @@ std::string PickleSerializer::serialize(std::map<std::string, std::string> metad
 
     return serialized;
 }
+
+std::string PickleSerializer::serializeProgrammes(std::map <uint16_t, std::string> metadata) {
+    // acquire GIL
+    PyGILState_STATE gstate;
+    gstate = PyGILState_Ensure();
+
+    PyObject* pickle = PyImport_ImportModule("pickle");
+    if (pickle == NULL) throw std::runtime_error("importing pickle module failed");
+
+    PyObject* programmeDict = PyDict_New();
+    if (programmeDict == NULL) {
+        Py_DECREF(pickle);
+        throw std::runtime_error("creating dictionary failed");
+    }
+
+    for (auto entry: metadata) {
+        PyObject* value = PyUnicode_DecodeUTF8(entry.second.c_str(), entry.second.length(), "replace");
+        if (value == NULL) {
+            if (PyErr_Occurred()) {
+                PyErr_PrintEx(0);
+            }
+            continue;
+        }
+
+        PyObject* key = PyLong_FromUnsignedLong(entry.first);
+        if (key == NULL) {
+            Py_DECREF(value);
+            if (PyErr_Occurred()) {
+                PyErr_PrintEx(0);
+            }
+            continue;
+        }
+
+        if (PyDict_SetItem(programmeDict, key, value) == -1) {
+            Py_DECREF(pickle);
+            Py_DECREF(programmeDict);
+            Py_DECREF(value);
+            throw std::runtime_error("failed to put item into dictionary");
+        }
+
+        Py_DECREF(value);
+    }
+
+    PyObject* dict = PyDict_New();
+    if (dict == NULL) {
+        Py_DECREF(pickle);
+        Py_DECREF(programmeDict);
+        throw std::runtime_error("creating dictionary failed");
+    }
+
+    if (PyDict_SetItemString(dict, "programmes", programmeDict) == -1) {
+        Py_DECREF(pickle);
+        Py_DECREF(programmeDict);
+        Py_DECREF(dict);
+        throw std::runtime_error("could not set dictionary key");
+    }
+
+    PyObject* result = PyObject_CallMethod(pickle, "dumps", "O", dict);
+    if (result == NULL) {
+        Py_DECREF(pickle);
+        Py_DECREF(dict);
+        throw std::runtime_error("failed to pickle dictionary");
+    }
+
+    Py_DECREF(dict);
+    Py_DECREF(pickle);
+
+    if (!PyBytes_Check(result)) {
+        Py_DECREF(result);
+        throw std::runtime_error("unexpected result (was expecting bytes)");
+    }
+
+    char* bytes;
+    Py_ssize_t length;
+    if (PyBytes_AsStringAndSize(result, &bytes, &length) == -1) {
+        Py_DECREF(result);
+        throw std::runtime_error("failed to extract result bytes");
+    }
+
+    std::string serialized(bytes, length);
+    Py_DECREF(result);
+
+    /* Release the thread. No Python API allowed beyond this point. */
+    PyGILState_Release(gstate);
+
+    return serialized;
+}
--- src/pickleserializer.hpp
+++ src/pickleserializer.hpp
@@ -7,6 +7,7 @@ namespace Csdr::Eti {
     class PickleSerializer: public Serializer {
         public:
             std::string serialize(std::map<std::string, std::string> data) override;
+            std::string serializeProgrammes(std::map<uint16_t, std::string> data) override;
     };
 
 }
